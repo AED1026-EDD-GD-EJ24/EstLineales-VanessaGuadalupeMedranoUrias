4ef971a4b2eb4380b477497fe5afdc25bb966b91
Vanessa Guadalupe Medrano Urías <147688736+VanessaMedranoUrias@users.noreply.github.com>
2024-04-19 16:44:39 +0000

    act

--- miPrincipal/DemoListaDeContactos.java
+++ miPrincipal/DemoListaDeContactos.java
@@ -7,18 +7,25 @@ public class DemoListaDeContactos {
         System.out.println("   LISTA DE CONTACTOS   ");
         System.out.println("************************");
         System.err.println();
-        ListaDeContactos lcd = new ListaDeContactos();
+        ListaDeContactos ldc = new ListaDeContactos();
         //agregamos algunos contactos
-        lcd.agregarContactos("Vanessa", "Medrano", "Cruz Galvez 3390", "vane.medrano@gmail.com", "01800","6677809010");
-        lcd.agregarContactos("Francisco", "Herrera", "Cruz Galvez 3392", "frank.herrera@gmail.com", "01802","6677809012");
-        lcd.agregarContactos("Paulina", "Garcia", "Cruz Galvez 3394", "pau.garcia@gmail.com", "01804","6677809014");
-        lcd.agregarContactos("Renata", "Gaxiola", "Cruz Galvez 3396", "Renata.gaxiola@gmail.com", "01806","6677809016");
+        ldc.agregarContactos("Vanessa", "Medrano", "Cruz Galvez 3390", "vane.medrano@gmail.com", "01800","6677809010");
+        ldc.agregarContactos("Francisco", "Herrera", "Cruz Galvez 3392", "frank.herrera@gmail.com", "01802","6677809012");
+        ldc.agregarContactos("Paulina", "Garcia", "Cruz Galvez 3394", "pau.garcia@gmail.com", "01804","6677809014");
+        ldc.agregarContactos("Renata", "Gaxiola", "Cruz Galvez 3396", "Renata.gaxiola@gmail.com", "01806","6677809016");
         
-        Lista<Contato> listado= ldc.mostrarTodosLosContactos();
+        Lista<Contacto> listado= ldc.mostrarTodosLosContactos();
         System.out.println("~Listado despues de la agregacion~");
         for (int i=0; i<listado.getTamanio(); i++){
             System.out.println(listado.getValor(i));
         }
-
+        if (ldc.eliminarContacto("Vanessa ", "Medrano")){
+            System.out.println("Contacto Eliminado");
+        }
+        listado=ldc.mostrarTodosLosContactos();
+        System.out.println("~Listado despues de la eliminacion~");
+        for (int i=0; i<listado.getTamanio(); i++){
+            System.out.println(listado.getValor(i));
+        }
     }
 }
